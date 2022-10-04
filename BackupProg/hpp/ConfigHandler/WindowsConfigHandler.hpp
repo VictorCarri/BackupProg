2c9c310a833abba077a600911d3e09a89d9be4c7
Victor Carri <victorcarri@gmail.com>
2022-10-04 19:45:36 -0400

    Reorganized the config handler so that there's a base class and a specialized class to handle Windows config.

---
+++ BackupProg/hpp/ConfigHandler/WindowsConfigHandler.hpp
@@ -0,0 +1,26 @@
+#ifndef WINDOWSCONFIGHANDLER_HPP
+#define WINDOWSCONFIGHANDLER_HPP
+
+	/* Boost or STL */
+	#include "bosmacros/filesystem.hpp" // FILESYSTEM_PATH
+
+	/* Our headers */
+	#include "ConfigHandler/ConfigHandler.hpp" // Base class
+
+	namespace windows
+	{
+		/*
+		* A configuration file handler that's specialized to handle Windows configuration.
+		*/
+		class ConfigHandler : public ConfigHandler
+		{
+			public:
+				/*
+				* \desc Constructor. Reads the entire JSON file in as a string, then uses Boost.JSON to parse it as JSON and save it.
+				* \param confFilePath The path to the config JSON file.
+				*/
+				ConfigHandler(FILESYSTEM_PATH confFilePath);
+		};
+	}
+
+#endif // WINDOWSCONFIGHANDLER_HPP
