be91b3fbaf01bc5ee1f880d057dd0acd2b457cda
Victor Carri <victorcarri@gmail.com>
2022-10-10 18:09:49 -0400

    Moved the code which parsed each backup path and its associated info to a dedicated container class.

---
+++ BackupProg/hpp/BackupPathInfo/BaseBackupPathInfo.hpp
@@ -0,0 +1,18 @@
+#ifndef BASEBACKUPPATHINFO_HPP
+#define BASEBACKUPPATHINFO_HPP
+
+	/*
+	* This namespace holds classes that store information about how to backup each path that we must back up.
+	*/
+	namespace backupInfo
+	{
+		/*
+		* Base class for classes that store backup info. Meant to be specialized by classes for each OS.
+		*/
+		class BaseBackupPathInfo
+		{
+
+		};
+	};
+
+#endif // BASEBACKUPPATHINFO_HPP
---
+++ BackupProg/hpp/BackupPathInfo/WindowsBackupPathInfo.hpp
@@ -0,0 +1,15 @@
+#ifndef WINDOWSBACKUPPATHINFO_HPP
+#define WINDOWSBACKUPPATHINFO_HPP
+
+	/* Our headers */
+	#include "BackupPathInfo/BaseBackupPathInfo.hpp" // Base class
+
+	namespace windows
+	{
+		class WindowsBackupPathInfo : public BaseBackupPathInfo
+		{
+
+		};
+	};
+
+#endif // !WINDOWSBACKUPPATHINFO_HPP
