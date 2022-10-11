23faff3d6de9b95a45fd061a44113ffeb9570219
Victor Carri <victorcarri@gmail.com>
2022-10-10 21:00:10 -0400

    Started working on the task object that creates the list of files to copy. However, I realized that I can start parallelizing tasks from here on out. So I'll create a master task class to handle a single path's backup, so that I can backup each path in parallel.

---
+++ BackupProg/hpp/CopyListCreator/BaseCopyListCreator.hpp
@@ -0,0 +1,11 @@
+#ifndef BASECOPYLISTCREATOR_HPP
+#define BASECOPYLISTCREATOR_HPP
+
+	/*
+	* Base class for all task classes that create the list of files and folders that we should copy to the backup drive.
+	*/
+	class BaseCopyListCreator
+	{
+	};
+
+#endif // BASECOPYLISTCREATOR_HPP
