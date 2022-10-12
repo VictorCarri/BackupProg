ce06cc721bcf0ab00a7d6692e21999e0361e067b
Victor Carri <victorcarri@gmail.com>
2022-10-12 17:54:36 -0400

    Started creating a trie to store directory paths. We don't need regexes for these paths, because they're always literal strings. Using a trie might allow the program to check for paths that should be skipped more efficiently.
    I need to change the operators to lambdas and use STL algorithms, because the operators only work on one level of the trie. I also need to think about how the prefix trie will work.

---
+++ BackupProg/hpp/PathTrie.hpp
@@ -0,0 +1,8 @@
+#ifndef PATHTRIE_HPP
+#define PATHTRIE_HPP
+
+class PathTrie
+{
+};
+
+#endif // PATHTRIE_HPP
---
+++ BackupProg/hpp/PathTrie/PathTrie.hpp
@@ -0,0 +1,14 @@
+#ifndef PATHTRIE_HPP
+#define PATHTRIE_HPP
+
+#define TRIE_ALPHABET_SIZE 256 // We only allow ASCII or UTF-8 paths
+
+/*
+* A data structure that efficiently stores path prefixes. Meant to be used to store directory paths that should be skipped, since directory paths that we should exclude are always plain strings,
+* not regexes.
+*/
+class PathTrie
+{
+};
+
+#endif // PATHTRIE_HPP
